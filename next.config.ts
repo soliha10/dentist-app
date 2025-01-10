baf6fb5b7c81a174b70ae70f3d2694a2c0c014c8
soliha10 <toxtamurodovagulshodaa@gmail.com>
2025-01-10 18:50:32 +0500

    Add some changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: "standalone", // Netlify uchun kerak
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 export default nextConfig;
