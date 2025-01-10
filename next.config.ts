6c7d5b4efc4594dbae1f1b54d100e0d7932656a9
soliha10 <toxtamurodovagulshodaa@gmail.com>
2025-01-10 18:59:27 +0500

    Add some packages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 const nextConfig = {
-  output: "standalone", // Netlify uchun kerak
-  reactStrictMode: true,
-  swcMinify: true,
+  output: "export",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/dentist-app/" : "",
 };
 
 export default nextConfig;
